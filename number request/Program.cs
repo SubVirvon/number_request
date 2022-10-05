01e9c053290ad17effc3d0fbdc6caf1a2f906a3f
Virvon <dan130205@gmail.com>
2022-10-05 14:23:20 +0300

    number request

---
+++ number request/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number_request
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = ConvertNumber();
+
+            Console.WriteLine($"Число {number} удалось сконвертировать в int");
+            Console.ReadKey();
+        }
+
+        static int ConvertNumber()
+        {
+            bool isSuccess = false;
+            string input;
+            int number = 0;
+            
+            while(isSuccess == false)
+            {
+                Console.Write("Введите число: ");
+                input = Console.ReadLine();
+                isSuccess = int.TryParse(input, out number);
+            }
+
+            return number;
+        } 
+    }
+}
